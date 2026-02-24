33e0a7ce74dc7f4c469cb2a563eaf612bb4e7952
DanielCleim <danielsgsmg@gmail.com>
2026-02-24 15:38:21 -0300

    Convertendo para Maiúsculo e para Minúscula

---
+++ src/StringRegex/ConvertendoMaiusculaMinuscula/Principal.java
@@ -0,0 +1,10 @@
+package StringRegex.ConvertendoMaiusculaMinuscula;
+
+public class Principal {
+    public static void main(String[] args) {
+        String texto = "Olá, Mundo!";
+        System.out.println(texto + "\n");
+        System.out.println(texto.toLowerCase()+ "\n");
+        System.out.println(texto.toUpperCase()+ "\n");
+    }
+}
