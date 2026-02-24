60e38062efbe46bc2430641d7bac4eaadda5a213
DanielCleim <danielsgsmg@gmail.com>
2026-02-24 14:37:39 -0300

    Iniciando curso praticando String e Regex, Exe: Removendo espaços em branco no início e fim

---
+++ src/StringRegex/RemovendoEspacos/Principal.java
@@ -0,0 +1,8 @@
+package StringRegex.RemovendoEspacos;
+
+public class Principal {
+    public static void main(String[] args) {
+        String nome = "    João Silva ";
+        System.out.println(nome.trim());
+    }
+}
