362cb6f1af205a7d82233b2efe6b0a3a535a4b8c
DanielCleim <danielsgsmg@gmail.com>
2026-02-26 08:32:00 -0300

    Criando uma lista com elementos únicos

--- src/ColecoesStreams/ElementosUnicos.java
+++ src/ColecoesStreams/ElementosUnicos.java
@@ -1,11 +1,13 @@
 package ColecoesStreams;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ElementosUnicos {
     public static void main(String[] args) {
-        Set<String> eventos = new HashSet<>();
+        List<String> eventos = new ArrayList<>();
 
         eventos.add("IA Conference Brasil");
         eventos.add("AI Summit");
@@ -14,6 +16,8 @@ public class ElementosUnicos {
         eventos.add("IA Conference Brasil");
         eventos.add("DevFest");
 
-        System.out.println("Lista de eventos: " + eventos);
+        Set<String> eventosUnicos = new HashSet<>(eventos);
+
+        System.out.println("Lista de eventos: " + eventosUnicos);
     }
 }
